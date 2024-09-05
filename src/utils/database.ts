1f0318f30bc2a8893786eab4d54a9077504f7e43
andysharn <sharma.abhisheknis@gmail.com>
2024-09-05 16:04:39 +0530

    final

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,12 +1,3 @@
-import mongoose from 'mongoose';
-export const connectDatabase =async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI as string);
-        console.log('mongoDB connected');
-    
-    }
-    catch(error){
-        console.error('mongoDB connection error',error)
-        process.exit(1);
-    }
-};
+import { PrismaClient} from "@prisma/client";
+const prisma =new PrismaClient();
+export default prisma;
